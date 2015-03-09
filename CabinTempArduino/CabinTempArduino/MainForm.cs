cef54634c200f2c91b0c4469b227a1b25e428f33
Martin Terjesen <martinterjesen@hotmail.com>
2015-03-09 13:45:49 +0100

    GUI

---
+++ CabinTempArduino/CabinTempArduino/MainForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CabinTempArduino
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            LimitsForm limitForm = new LimitsForm();
+            limitForm.ShowDialog();
+        }
+
+        private void btnSubscribers_Click(object sender, EventArgs e)
+        {
+            SubscribersForm subscribersForm = new SubscribersForm();
+            subscribersForm.ShowDialog();
+        }
+    }
+}
