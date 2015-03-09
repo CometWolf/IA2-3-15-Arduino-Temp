43339a3ce3be31421d275b34ed42b512936ac852
Martin Terjesen <martinterjesen@hotmail.com>
2015-03-09 13:53:42 +0100

    Gui

--- CabinTempArduino/CabinTempArduino/LimitsForm.Designer.cs
+++ CabinTempArduino/CabinTempArduino/LimitsForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+<<<<<<< HEAD
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.button1 = new System.Windows.Forms.Button();
@@ -56,15 +57,41 @@
             this.button1.TabIndex = 3;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+=======
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Upper limit";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(133, 64);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+>>>>>>> parent of 6eb3e11... GUI
             // 
             // LimitsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+<<<<<<< HEAD
             this.Controls.Add(this.button1);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.textBox1);
+=======
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+>>>>>>> parent of 6eb3e11... GUI
             this.Name = "LimitsForm";
             this.Text = "LimitsForm";
             this.ResumeLayout(false);
@@ -74,8 +101,13 @@
 
         #endregion
 
+<<<<<<< HEAD
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Button button1;
+=======
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+>>>>>>> parent of 6eb3e11... GUI
     }
 }
