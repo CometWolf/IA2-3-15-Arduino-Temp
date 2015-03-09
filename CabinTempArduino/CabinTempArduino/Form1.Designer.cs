606d9b1b0ab238a721ca618ce4ea5656d7b2ce50
Martin Terjesen <martinterjesen@hotmail.com>
2015-03-09 13:06:46 +0100

    GUI

--- CabinTempArduino/CabinTempArduino/Form1.Designer.cs
+++ CabinTempArduino/CabinTempArduino/Form1.Designer.cs
@@ -28,39 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.grbBatteryStatus = new System.Windows.Forms.GroupBox();
+            this.prbBatteryStatus = new System.Windows.Forms.ProgressBar();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.lblBatteryStatus = new System.Windows.Forms.Label();
+            this.rtbDatabaseValues = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtFetchLast = new System.Windows.Forms.TextBox();
+            this.btnFetch = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.grbBatteryStatus.SuspendLayout();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // groupBox1
+            // grbBatteryStatus
             // 
-            this.groupBox1.Controls.Add(this.label3);
-            this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.progressBar1);
-            this.groupBox1.Location = new System.Drawing.Point(12, 237);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(514, 51);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Battery Status";
+            this.grbBatteryStatus.Controls.Add(this.lblBatteryStatus);
+            this.grbBatteryStatus.Controls.Add(this.label2);
+            this.grbBatteryStatus.Controls.Add(this.prbBatteryStatus);
+            this.grbBatteryStatus.Location = new System.Drawing.Point(12, 320);
+            this.grbBatteryStatus.Name = "grbBatteryStatus";
+            this.grbBatteryStatus.Size = new System.Drawing.Size(465, 51);
+            this.grbBatteryStatus.TabIndex = 0;
+            this.grbBatteryStatus.TabStop = false;
+            this.grbBatteryStatus.Text = "Battery Status";
             // 
-            // progressBar1
+            // prbBatteryStatus
             // 
-            this.progressBar1.Location = new System.Drawing.Point(6, 19);
-            this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(378, 23);
-            this.progressBar1.TabIndex = 1;
+            this.prbBatteryStatus.Location = new System.Drawing.Point(6, 19);
+            this.prbBatteryStatus.Name = "prbBatteryStatus";
+            this.prbBatteryStatus.Size = new System.Drawing.Size(378, 23);
+            this.prbBatteryStatus.TabIndex = 1;
             // 
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(12, 24);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(114, 20);
             this.textBox1.TabIndex = 1;
             // 
             // label1
@@ -81,25 +94,150 @@
             this.label2.TabIndex = 3;
             this.label2.Text = "Status: ";
             // 
+            // lblBatteryStatus
+            // 
+            this.lblBatteryStatus.AutoSize = true;
+            this.lblBatteryStatus.Location = new System.Drawing.Point(430, 19);
+            this.lblBatteryStatus.Name = "lblBatteryStatus";
+            this.lblBatteryStatus.Size = new System.Drawing.Size(27, 13);
+            this.lblBatteryStatus.TabIndex = 4;
+            this.lblBatteryStatus.Text = "N/A";
+            // 
+            // rtbDatabaseValues
+            // 
+            this.rtbDatabaseValues.Location = new System.Drawing.Point(132, 24);
+            this.rtbDatabaseValues.Name = "rtbDatabaseValues";
+            this.rtbDatabaseValues.Size = new System.Drawing.Size(345, 290);
+            this.rtbDatabaseValues.TabIndex = 3;
+            this.rtbDatabaseValues.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(129, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(168, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Fetched values from the database";
+            // 
+            // txtFetchLast
+            // 
+            this.txtFetchLast.Location = new System.Drawing.Point(6, 38);
+            this.txtFetchLast.Name = "txtFetchLast";
+            this.txtFetchLast.Size = new System.Drawing.Size(101, 20);
+            this.txtFetchLast.TabIndex = 5;
+            // 
+            // btnFetch
+            // 
+            this.btnFetch.Location = new System.Drawing.Point(6, 154);
+            this.btnFetch.Name = "btnFetch";
+            this.btnFetch.Size = new System.Drawing.Size(101, 23);
+            this.btnFetch.TabIndex = 6;
+            this.btnFetch.Text = "Fetch values";
+            this.btnFetch.UseVisualStyleBackColor = true;
+            // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(430, 19);
+            this.label3.Location = new System.Drawing.Point(3, 22);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(27, 13);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "N/A";
+            this.label3.Size = new System.Drawing.Size(94, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "How many values:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 261);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Limits";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 290);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(114, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Subscribers";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 87);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(82, 17);
+            this.radioButton1.TabIndex = 10;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Temperatue";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 110);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(47, 17);
+            this.radioButton2.TabIndex = 11;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Error";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(6, 133);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(101, 17);
+            this.radioButton3.TabIndex = 12;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Temp. and Error";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 71);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "What to fetch:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtFetchLast);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.btnFetch);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 61);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(114, 183);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Fetching values";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(664, 300);
+            this.ClientSize = new System.Drawing.Size(488, 381);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.rtbDatabaseValues);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.grbBatteryStatus);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.grbBatteryStatus.ResumeLayout(false);
+            this.grbBatteryStatus.PerformLayout();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
@@ -109,12 +247,24 @@
 
         #endregion
 
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.GroupBox grbBatteryStatus;
+        private System.Windows.Forms.ProgressBar prbBatteryStatus;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblBatteryStatus;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox rtbDatabaseValues;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtFetchLast;
+        private System.Windows.Forms.Button btnFetch;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
 
