b7e39db1cd757d956a94fb5f90d2bf18ff9bf525
JorundMartinsen <joerundmartinsen@hotmail.com>
2015-03-10 21:02:00 +0100

    La til Database.cs og OLEdb.cs

---
+++ PC/CabinTempArduino/Database.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.OleDb;
+
+namespace CabinTempArduino
+{
+    /// <summary>
+    /// Connects to a database and simplifies communication
+    /// </summary>
+    class Database:OLEdb
+    {
+        /// Initializes an instance of the Database class
+      /// </summary>
+      /// <param name="DbPath">Path to Database</param>
+        public Database(string DbPath) : base(DbPath)
+        {
+            
+        }
+        /// <summary>
+        /// Adds a subscriber to BrukerInformasjon
+        /// </summary>
+        /// <param name="surName"></param>
+        /// <param name="firstName"></param>
+        /// <param name="userName"></param>
+        /// <param name="password"></param>
+        /// <param name="email"></param>
+        /// <param name="phone"></param>
+        public void AddSubscriber(string surName, string firstName, string userName, string password, string email, string phone)
+        {
+            string table = "BrukerInformasjon";
+
+            try
+            {
+
+                OpenDb(table);
+
+                DataRow row = myDataset.Tables[table].NewRow();
+
+                row["Etternavn"] = surName;
+                row["Fornavn"] = firstName;
+                row["Brukernavn"] = userName;
+                row["Passord"] = password;
+                row["E-post"] = email;
+                row["Telefon"] = phone;
+
+                myDataset.AcceptChanges();
+                myDataset.Tables[table].Rows.Add(row);
+
+                CloseDb(table);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                myAccessConnection.Close();
+            }
+        }
+        /// <summary>
+        /// Deletes a row from BrukerInformasjon
+        /// </summary>
+        /// <param name="userID">Row to delete</param>
+        public void DeleteSubscriber(int userID)
+        {
+            string table="Brukerinformasjon";
+            try
+            {
+
+                OpenDb(table);
+
+                myDataset.Tables[table].Rows[userID].Delete();
+
+                CloseDb(table);
+
+            }
+            catch (Exception ex)
+            {
+                
+                throw ex;
+            }
+            finally
+            {
+                myAccessConnection.Close();
+            }
+        }
+        /// <summary>
+        /// Gets all subscribers in 'BrukerInformasjon'
+        /// </summary>
+        /// <returns>
+        /// Dynamic string array of all values in 'BrukerInformasjon'
+        /// </returns>
+        public string[,] GetSubscribers()
+        {
+            string[,] subscribers;
+            string table = "BrukerInformasjon";
+            try
+            {
+                OpenDb(table);
+
+                int rows = myDataset.Tables[table].Rows.Count;
+                int columns = myDataset.Tables[table].Columns.Count;
+
+                subscribers = new string[rows, columns];
+
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < columns; j++)
+                    {
+                        subscribers[i, j] = myDataset.Tables[table].Rows[i].ItemArray[j].ToString();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                subscribers = new string[0, 0];
+                subscribers[0, 0] = "N/A";
+
+                throw ex;
+            }
+            finally
+            {
+                myAccessConnection.Close();
+            }
+            return subscribers;
+        }
+        /// <summary>
+        /// Adds an alarm to Feilmeldingslogg
+        /// </summary>
+        /// <param name="date"></param>
+        /// <param name="time"></param>
+        /// <param name="message"></param>
+        /// <param name="alarmID"></param>
+        /// <param name="temp"></param>
+        public void LogAlarm(string date, string time, string message, string alarmID, string temp)
+        {
+            string table = "Feilmeldingslogg";
+
+            try
+            {
+
+                OpenDb(table);
+
+                DataRow row = myDataset.Tables[table].NewRow();
+
+                row["Dato"] = date;
+                row["Tid"] = time;
+                row["Feilmelding"] = message;
+                row["AlarmID"] = alarmID;
+                row["Temperatur"] = temp;
+
+                myDataset.AcceptChanges();
+                myDataset.Tables[table].Rows.Add(row);
+
+                CloseDb(table);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                myAccessConnection.Close();
+            }
+        }
+        /// <summary>
+        /// Gets all alarms in 'Feilmeldingslogg'
+        /// </summary>
+        /// <returns>
+        /// Dynamic string array of all values in 'Feilmeldingslogg'
+        /// </returns>
+        public string[,] GetAlarm()
+        {
+            string[,] alarms;
+            string table = "Feilmeldingslogg";
+            try
+            {
+                OpenDb(table);
+                int rows = myDataset.Tables[table].Rows.Count;
+                int columns = myDataset.Tables[table].Columns.Count;
+                alarms = new string[rows, columns];
+
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < columns; j++)
+                    {
+                        alarms[i, j] = myDataset.Tables[table].Rows[i].ItemArray[j].ToString();                        
+                    }
+                }
+                CloseDb(table);
+            }
+            catch (Exception ex)
+            {
+                alarms = new string[0, 0];
+                alarms[0, 0] = "N/A";
+
+                throw ex;
+            }
+            finally
+            {
+                myAccessConnection.Close();
+            }
+            return alarms;
+        }
+        /// <summary>
+        /// Adds a row to TemperaturLogg
+        /// </summary>
+        /// <param name="date"></param>
+        /// <param name="time"></param>
+        /// <param name="temp"></param>
+        public void LogTemperature(string date, string time, string temp)
+        {
+            string table = "TemperaturLogg";
+
+            try
+            {
+
+                OpenDb(table);
+
+                DataRow row = myDataset.Tables[table].NewRow();
+
+                row["Dato"] = date;
+                row["Tid"] = time;
+                row["Temperatur"] = temp;
+
+                myDataset.AcceptChanges();
+                myDataset.Tables[table].Rows.Add(row);
+
+                CloseDb(table);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                myAccessConnection.Close();
+            }
+        }
+        public string[,] GetTemperature()
+        {
+            string[,] temperature;
+            string table = "Feilmeldingslogg";
+            try
+            {
+                OpenDb(table);
+                int rows = myDataset.Tables[table].Rows.Count;
+                int columns = myDataset.Tables[table].Columns.Count;
+                temperature = new string[rows, columns];
+
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < columns; j++)
+                    {
+                        temperature[i, j] = myDataset.Tables[table].Rows[i].ItemArray[j].ToString();
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                temperature = new string[0, 0];
+                temperature[0, 0] = "N/A";
+
+                throw ex;
+            }
+            finally
+            {
+                myAccessConnection.Close();
+            }
+            return temperature;
+        }
+    }
+}
---
+++ PC/CabinTempArduino/OLEdb.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.OleDb;
+
+namespace CabinTempArduino
+{
+    class OLEdb
+    {
+        protected static OleDbConnection myAccessConnection;
+        protected static OleDbCommand myAccessCommand;
+        protected static OleDbDataAdapter myDataAdapter;
+        protected static OleDbCommandBuilder myCommandBuilder;
+        protected static DataSet myDataset;
+        static string connectionString;
+        /// <summary>
+        /// Initializes an instance of the OLEDB class
+        /// Provider =  Microsoft.ACE.OLEDB.12.0
+        /// </summary>
+        /// <param name="DbPath">Path to the database</param>
+        public OLEdb(string DbPath)
+        {
+            connectionString =
+                "Provider=Microsoft.ACE.OLEDB.12.0;" +
+                "Data Source=" +
+                DbPath +
+                ";" +
+                "Persist Security Info=False";
+            try
+            {
+                myAccessConnection = new OleDbConnection(connectionString);
+                myAccessCommand = new OleDbCommand("SELECT * FROM Brukerinformasjon", myAccessConnection);
+
+                myDataAdapter = new OleDbDataAdapter(myAccessCommand);
+
+                myDataset = new DataSet();
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+        /// <summary>
+        /// Opens a connection to the database
+        /// and reads the values i table "table" to a dataset
+        /// </summary>
+        /// <param name="table">Table to read</param>
+        protected static void OpenDb(string table)
+        {
+            myAccessCommand.CommandText = "SELECT * FROM " + table;
+            myDataset = new DataSet();
+            myAccessConnection.Open();
+            myDataAdapter.Fill(myDataset, table);
+        }
+        /// <summary>
+        /// Commits the values in dataset to the database
+        /// and closes the connection
+        /// </summary>
+        /// <param name="table">Table to commit</param>
+        protected static void CloseDb(string table)
+        {
+            myCommandBuilder = new OleDbCommandBuilder(myDataAdapter);
+            myCommandBuilder.QuotePrefix = "[";
+            myCommandBuilder.QuoteSuffix = "]";
+
+            myDataAdapter.UpdateCommand = myCommandBuilder.GetUpdateCommand();
+
+            myDataAdapter.Update(myDataset.Tables[table]);
+            myAccessConnection.Close();
+        }
+        
+    }
+}
