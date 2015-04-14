129f62d313993796482cdcf666353a89f2e92132
oysteinen <oystein.lorentz@gmail.com>
2015-04-14 21:10:01 +0200

    Endringer i main

--- Arduino/Main/Main.ino
+++ Arduino/Main/Main.ino
@@ -2,70 +2,81 @@
 #include<Display.h>
 #include<PC.h>
 #include<TempSensor.h>
+#include<Relay.h>
 #include<stdlib.h>
+#include<LiquidCrystal.h>
 
 const byte tempPin = 9;
 const byte furnacePin = 8;
+
+String alarmUpper;
+String alarmLower;
+String furnaceLower;
+String furnaceUpper;
 
 float alarmUpperLimit;
 float alarmLowerLimit;
 float furnaceUpperLimit;
 float furnaceLowerLimit;
 float tempValue;
+char temp[5];
 
-char numChar[6]
-String temp = "";
+PC pc = PC();
+Display lcd(2,3,4,5);
+TempSensor tempSensor(tempPin, alarmUpperLimit, alarmLowerLimit);
+Furnace furnace(20, -10, furnacePin);
 
-void setup()
-{ 
-  PC pc();
- 
-  // Gathers upper and lower temperature limits using the PC class.
-  // A character is given to get the right value from the main program.
-  alarmUpperLimit = (pc.receive('U')).toFloat();
-  alarmLowerLimit = (pc.receive('L')).toFloat();
-  furnaceUpperLimit = (pc.receive('W')).toFloat();
-  furnaceLowerLimit = (pc.receive('I')).toFloat();
-   
-  TempSensor tempSensor(tempPin, alarmUpperLimit, alarmLowerLimit);
-  Furnace furnace(furnaceUpperLimit, furnaceLowerLimit, furnacePin);
-  Display lcd(2,3,4,5);
-  
+void setup() 
+{
   pc.begin(9600);
-  Serial.begin(9600);
-}  
-loop()
-{
-  delay(1000);
-  
-  tempValue = tempSensor.getTemp();
-  
-  dtostrf(tempValue, 4, 3, numChar);      //Used to convert tempValue's digits to chars and assigns them to an array.
-  
-  for(int i = 0; i < sizeof(numChar); i++)    //inserts the chars into a string. 
-  {
-	  temp += numChar[i];
-  }
-  
-  lcd.Write(temp); 		//Sends the temp string to the display
-  Alarm(tempValue);		//Calls on alarm method
-  furnace.update(tempValue); 	//Keeps the furnace updated on tempValue
-  if(pc.receive == "temp")
-  {
-    pc.send(temp);
-  }  
 }
 
-void Alarm(float temp)
+void loop() 
 {
-// If the temperature goes above or under the assorted values, 
-// a string of text will be sent to the main program. 	
-  if (tempValue >= alarmUpperLimit)
+  alarmUpperLimit = alarmUpper.toFloat();
+  alarmLowerLimit = alarmLower.toFloat();
+  furnaceUpperLimit = furnaceUpper.toFloat();
+  furnaceLowerLimit = furnaceLower.toFloat();
+
+  tempValue = random(-17,27);
+  dtostrf(tempValue, 5, 1, temp); 
+  String motatt = pc.receive('\n');
+  String handling = motatt.substring(0,4);
+  String value = motatt.substring(4,9);
+  
+    if(motatt != "")
   {
-	  pc.send("ALARM_UPPER");
-  }
-  if else (tempValue <= alarmLowerLimit)
-  {
-	  pc.send("ALARM_LOWER  ");
+    if(handling == "TEMP")
+    {
+      pc.send(temp);
+    }
+    else if(handling == "ALUP")
+    {
+      alarmUpper = value;
+    }
+    else if(handling == "ALLO")
+    {
+      alarmLower = value;
+    }
+    else if(handling == "FUUP")
+    {
+      furnaceUpper = value;
+    }
+    else if(handling == "FULO")
+    {
+      furnaceLower = value;
+    }
+    else if(handling == "ALRM")
+    {
+      if(tempValue < alarmLowerLimit)
+      {
+        pc.send("ALARM_LOW");
+      }
+      else if (tempValue > alarmUpperLimit)
+      {
+        pc.send("ALARM_UP");
+      }
+    }
   }
 }
+
