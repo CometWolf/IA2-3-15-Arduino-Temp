28be525cdafaed2bbc8f036cd8c43eea1792adbf
Haakon N. Unelsrød <paukatt@gmail.com>
2015-04-15 16:44:37 +0200

    Initial website commit

---
+++ Nettside/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.Mvc;
+using WebApplication6.Models;
+
+namespace WebApplication6.Controllers {
+    public class HomeController : Controller {
+        private Database database = new Database("C:\\Users\\Haakon\\Documents\\Database5.accdb");
+        private void CheckLogin() { //Check if user is logged in
+            if (!User.Identity.IsAuthenticated) {
+                //Not logged in, redirect to login page
+                //Response.Redirect("~/Account/Login");
+            }
+        }
+
+        [HttpPost]
+        public JsonResult GetTemp() {
+            return Json(new { result = "22" }, JsonRequestBehavior.AllowGet);//database.GetTemperatureLast(1);
+        }
+        public string GetTemp(bool stringReturn) {
+            return "25";//database.GetTemperatureLast(1);
+        }
+
+        [HttpPost]
+        public void UpdateAlarmLimits(string upperLimit, string lowerLimit, string updateInterval) {
+            database.UpdateSetting(upperLimit, 1, 0);
+            database.UpdateSetting(lowerLimit, 3, 0);
+            database.UpdateSetting(updateInterval, 5, 0);
+            Response.Redirect("~/");
+        }
+
+        [HttpPost]
+        public void UpdateFurnaceLimits(string upperLimit, string lowerLimit) {
+            database.UpdateSetting(upperLimit, 2, 0);
+            database.UpdateSetting(lowerLimit, 4, 0);
+            Response.Redirect("~/");
+        }
+        
+        public ActionResult Index() {
+            CheckLogin();
+            ViewBag.Title = "Hovedside";
+            //temperature
+            ViewBag.temperature = GetTemp(true);
+            //settings
+            string[] setting = database.GetSettings();
+            ViewBag.highAlarm = setting[1];
+            ViewBag.lowAlarm = setting[3];
+            ViewBag.furnaceHigh = setting[2];
+            ViewBag.furnaceLow = setting[4];
+            ViewBag.updateInterval = setting[5];
+            //alarm
+            string[,] alarm = database.GetAlarmLast(1);
+            /*if (alarm[0, 6] == "0") { //alarm not signed
+                ViewBag.alarm = "\n" + alarm[0,4];
+            } //else no unsigned alarm */
+            return View();
+        }
+
+        public ActionResult Logg() {
+            CheckLogin();
+            ViewBag.Title = "Logg";
+            ViewBag.Message = "Alarm og temperatur logg";
+            return View();
+        }
+    }
+}
---
+++ Nettside/Models/OLEdb.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.OleDb;
+
+namespace WebApplication6.Models
+{
+    class OLEdb
+    {
+        protected static OleDbConnection myAccessConnection;
+        protected static OleDbCommand myAccessCommand;
+        protected static OleDbDataAdapter myDataAdapter;
+        protected static OleDbCommandBuilder myCommandBuilder;
+        protected static DataTable myDatatable;
+        static string connectionString;
+        /// <summary>
+        /// Initializes an instance of the OLEDB class
+        /// Provider =  Microsoft.ACE.OLEDB.12.0
+        /// </summary>
+        /// <param name="DbPath">Path to the database</param>
+        public OLEdb(string DbPath)
+        {
+            connectionString =
+                "Provider=Microsoft.ACE.OLEDB.12.0;" +
+                "Data Source=" +
+                DbPath +
+                ";" +
+                "Persist Security Info=False";
+            try
+            {
+                myAccessConnection = new OleDbConnection(connectionString);
+                myAccessCommand = new OleDbCommand("SELECT * FROM Brukerinformasjon", myAccessConnection);
+
+                myDataAdapter = new OleDbDataAdapter(myAccessCommand);
+
+                myDatatable = new DataTable();
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+        /// <summary>
+        /// Runs any given query
+        /// </summary>
+        /// <param name="query">Query to run</param>
+        public void RunQuery(string query)
+        {
+            try
+            {
+                myAccessConnection.Open();
+                myAccessCommand = new OleDbCommand(query, myAccessConnection);
+                myAccessCommand.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                myAccessConnection.Close();
+            }
+        }
+        /// <summary>
+        /// Opens a connection to the database
+        /// and reads the values in table "table" to a dataset
+        /// </summary>
+        /// <param name="table">Table to read</param>
+        protected static void OpenDb(string table)
+        {
+            myAccessCommand.CommandText = string.Format("SELECT * FROM {0}", table);
+            myDatatable = new DataTable();
+            myAccessConnection.Open();
+            myDataAdapter.Fill(myDatatable);
+            
+        }
+        /// <summary>
+        /// Opens a connection to the database
+        /// and reads the chosen values in table "table" to a dataset
+        /// </summary>
+        /// <param name="table">Table to read</param>
+        /// <param name="value">Values to select as SQL</param>
+        protected static void OpenDb(string table, string value)
+        {
+            myAccessCommand.CommandText = string.Format("SELECT {0} FROM {1}", value, table);
+            myDatatable = new DataTable();
+            myAccessConnection.Open();
+            myDataAdapter.Fill(myDatatable);
+        }
+        protected static void OpenDbMan(string selectQuery)
+        {
+            myAccessCommand.CommandText = selectQuery;
+            myDatatable = new DataTable();
+            myAccessConnection.Open();
+            myDataAdapter.Fill(myDatatable);
+        }
+        /// <summary>
+        /// Commits the values in dataset to the database
+        /// and closes the connection
+        /// </summary>
+        /// <param name="table">Table to commit</param>
+        protected static void CloseDb(string table)
+        {
+            myCommandBuilder = new OleDbCommandBuilder(myDataAdapter);
+            myCommandBuilder.QuotePrefix = "[";
+            myCommandBuilder.QuoteSuffix = "]";
+
+            myDataAdapter.UpdateCommand = myCommandBuilder.GetUpdateCommand();
+
+            myDataAdapter.Update(myDatatable);
+            myAccessConnection.Close();
+        }
+        
+    }
+}
