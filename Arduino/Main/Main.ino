95e5ce5b3daca1ea43948f610c3d2db6f12da92b
oysteinen <oystein.lorentz@gmail.com>
2015-04-15 16:20:54 +0200

    Endringer i main

--- Arduino/Main/Main.ino
+++ Arduino/Main/Main.ino
@@ -1,12 +1,11 @@
 #include<Furnace.h>
-#include<Display.h>
 #include<PC.h>
 #include<TempSensor.h>
 #include<Relay.h>
 #include<stdlib.h>
 #include<LiquidCrystal.h>
 
-const byte tempPin = 9;
+const int tempPin = A0; 
 const byte furnacePin = 8;
 
 String alarmUpper;
@@ -20,31 +19,44 @@ float furnaceUpperLimit;
 float furnaceLowerLimit;
 float tempValue;
 char temp[5];
+char displayChar[5];
 
 PC pc = PC();
-Display lcd(2,3,4,5);
-TempSensor tempSensor(tempPin, alarmUpperLimit, alarmLowerLimit);
+TempSensor tempSensor(tempPin);
 Furnace furnace(20, -10, furnacePin);
+LiquidCrystal lcd(12,11,5,4,3,2);
 
 void setup() 
 {
   pc.begin(9600);
+  lcd.begin(16,2);
+  lcd.print("Origo 2. etasje!");
+  lcd.setCursor(0,1);
+  lcd.print("Temp:");
+  lcd.setCursor(12,1);
+  lcd.print("C");
 }
 
 void loop() 
 {
+  
   alarmUpperLimit = alarmUpper.toFloat();
   alarmLowerLimit = alarmLower.toFloat();
+  
   furnaceUpperLimit = furnaceUpper.toFloat();
   furnaceLowerLimit = furnaceLower.toFloat();
-
-  tempValue = random(-17,27);
-  dtostrf(tempValue, 5, 1, temp); 
+  furnace.upperLimit = furnaceUpperLimit;
+  furnace.lowerLimit = furnaceLowerLimit;
+  
   String motatt = pc.receive('\n');
   String handling = motatt.substring(0,4);
   String value = motatt.substring(4,9);
   
-    if(motatt != "")
+  tempValue = tempSensor.getTemp();
+  dtostrf(tempValue, 5, 1, temp); 
+  furnace.update(tempValue);
+  
+  if(motatt != "")
   {
     if(handling == "TEMP")
     {
@@ -76,7 +88,24 @@ void loop()
       {
         pc.send("ALARM_UP");
       }
-    }
+    } 
   }
+  
+  float tempFloor = floor(tempValue);
+  float rest = tempValue - tempFloor;
+  
+  if((rest == 0)||(rest < 0.25)){
+  	tempValue = tempFloor;
+  }
+  else if ((rest >= 0.25)||(rest < 0.75)){
+  	tempValue = tempFloor + 0.5;
+  }
+  else if (rest >= 0.75){
+  	tempValue = ceil(tempValue);
+  }
+  
+  dtostrf(tempValue, 5, 1, displayChar); 
+  String tempDisplay(displayChar);
+  lcd.setCursor(6,1);
+  lcd.print(tempDisplay);
 }
-
