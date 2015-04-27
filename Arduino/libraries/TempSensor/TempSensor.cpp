753f344772af0913e57499a025d31a03465c8083
Haakon N. Unelsrød <paukatt@gmail.com>
2015-04-27 12:40:59 +0200

    Moved Arduino library folder

--- Arduino/libraries/TempSensor/TempSensor.cpp
+++ Arduino/libraries/TempSensor/TempSensor.cpp
@@ -1,20 +1,12 @@
-/*
-  Written by: Martin Terjesen
-  Arudino temperature sensor class. Define the temperature measured at 5v as max, and the temperature at 0v as min in the constructor.
-*/
-
 #include "TempSensor.h"
 
-TempSensor::TempSensor(byte pin,int min, int max)
+TempSensor::TempSensor(int pin)
 {
 	this->pin = pin;
-  this->min = min;
-  this->range = max-min; //temperature resolution
 };
 float TempSensor::getTemp()
 {
-  int analogIn = analogRead(pin);
-  float inPercentage = (float)analogIn/(float)1023; //convert in signal to percentage
-  float temp = range*inPercentage+min; //Convert from in percentage temperature based on temperature range
-	return temp;
+  
+	return ((((analogRead(pin)*5.0) / 1024) - 0.5) * 100);
+	
 };
